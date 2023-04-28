f87971a2321e79f9112c210b5d64c3eaf0373258
Reynaflor Sentillas <reynaflorsentillas@ymail.com>
2023-04-29 02:08:53 +0800

    update

---
+++ bahia_doc_mig/__openerp__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Bahia Document Migration',
+    'category': 'Extra Tools',
+    'sequence': 25,
+    'summary': 'Bahia Document Migration',
+    'description': 'Bahia Document Migration',
+    'author': 'Excode Innovation Solutions Inc.',    
+    'depends': ['hr'],
+    'application': False,
+}
