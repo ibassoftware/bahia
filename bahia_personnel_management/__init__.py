3f8d7f43c3506e7b0dc1965fa071a177de008e85
Reynaflor Sentillas <reynaflorsentillas@ymail.com>
2023-04-29 20:02:03 +0800

    Update __init__.py

--- bahia_personnel_management/__init__.py
+++ bahia_personnel_management/__init__.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
+from . import controllers
 import hr_parameter_model
 import hr_recruitment_seabased
 import user_defined_exception
 import hr_recruitment
 import reports
-import controllers
