0915fb963bdacf43d13eb307e3be71ecbcc2dd93
Reynaflor Sentillas <reynaflorsentillas@ymail.com>
2023-04-29 20:04:58 +0800

    Update __init__.py

--- bahia_personnel_management/__init__.py
+++ bahia_personnel_management/__init__.py
@@ -1,10 +1,13 @@
 # -*- encoding: utf-8 -*-
+import sys
+
 # Mock deprecated openerp.addons.web.http module
 import openerp.http
 sys.modules['openerp.addons.web.http'] = openerp.http
 http = openerp.http
 
 from . import controllers
+
 import hr_parameter_model
 import hr_recruitment_seabased
 import user_defined_exception
