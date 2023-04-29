207f29247aa0665ab2da07a7d36b4c5d1289dd7d
Reynaflor Sentillas <reynaflorsentillas@ymail.com>
2023-04-29 18:23:49 +0800

    Update hr_employee.py

--- bahia_doc_mig/models/hr_employee.py
+++ bahia_doc_mig/models/hr_employee.py
@@ -10,9 +10,9 @@ class HrEmployeeExtend(models.Model):
 
     @api.multi 
     def get_file_personal_data(self):
-    	_logger.info("TESST")
-    	return {
-    		'type' : 'ir.actions.act_url',
-    		'url': '/web/binary/download_document?model=hr.employee&field=legacy_doc_2&id=%s&filename=filename2'%(self.id),
-    		'target': 'self',
-    	}
+        _logger.info("TESST")
+        return {
+            'type' : 'ir.actions.act_url',
+            'url': '/web/binary/download_document?model=hr.employee&field=legacy_doc_2&id=%s&filename=filename2'%(self.id),
+            'target': 'self',
+        }
