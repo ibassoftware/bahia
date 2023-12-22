4658d22e47a83d74ac0aa542fbaa9e346418984c
Reynaflor Sentillas <reynaflorsentillas@ymail.com>
2023-12-22 20:04:37 +0800

    Update hr_data_migration.py

--- ibas_bahia_migration/models/hr_data_migration.py
+++ ibas_bahia_migration/models/hr_data_migration.py
@@ -5,3 +5,15 @@ class DataMigration(models.TransientModel):
 	_name ='hr.config.data.migration'
 
 	name = fields.Char('Name', default='Data Migration Name')
+
+	def changeEmployeeName(self):
+		employee_model = self.env['hr.employee'].search([])
+		
+		for employee in employee_model:
+			if employee.first_name == False:
+				employee.first_name=''
+			if employee.middle_name == False:
+				employee.middle_name=''
+			if employee.last_name == False:
+				employee.last_name=''			
+			employee.name =  employee.last_name + ", " +  employee.first_name + " " + employee.middle_name   
