a0454a04a409c36b66f01903a38bbadeb4dafa8d
Reynaflor Sentillas <reynaflorsentillas@ymail.com>
2023-12-22 22:23:22 +0800

    Update run_hr_employee_image.py

--- ibas_bahia_migration/api/run_hr_employee_image.py
+++ ibas_bahia_migration/api/run_hr_employee_image.py
@@ -39,7 +39,7 @@ def update_employee_image():
 	count = 0
 	count_update = 0
 
-	args = [('id', '=', 50382)]
+	args = [('id', '=', 54049)]
 	get_employee = src_models.execute(src_DB, src_uid, src_PASS, 'hr.employee', 'search', args)
 
 	if get_employee:
