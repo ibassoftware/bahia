6264578c248fd9114e91682d51baa0bc14d2042f
Reynaflor Sentillas <reynaflorsentillas@ymail.com>
2023-12-22 21:08:05 +0800

    Update hr_employee.py

--- ibas_bahia_migration/models/hr_employee.py
+++ ibas_bahia_migration/models/hr_employee.py
@@ -7,10 +7,25 @@ class HrEmployeeMigration(models.Model):
 	# For API use
 	def updateEmployeeName(self):
 		for employee in self:
+			first_name = ''
+			middle_name = ''
+			last_name = ''
+
 			if employee.first_name == False:
 				employee.first_name=''
+			else:
+				first_name = employee.first_name
+
 			if employee.middle_name == False:
 				employee.middle_name=''
+			else:
+				middle_name = employee.middle_name
+
 			if employee.last_name == False:
-				employee.last_name=''			
-			employee.name =  employee.last_name + ", " +  employee.first_name + " " + employee.middle_name
+				employee.last_name=''
+			else:
+				last_name = employee.last_name
+
+			if first_name or middle_name or last_name:
+				employee.name =  last_name + ", " +  employee.first_name + " " + employee.middle_name
+		return True
