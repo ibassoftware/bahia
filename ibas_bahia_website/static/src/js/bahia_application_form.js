b9b93ee572b4eb416924dc21bc2d177edc3c2fe6
Reynaflor Sentillas <reynaflorsentillas@ymail.com>
2024-01-26 21:31:49 +0800

    Update bahia_application_form.js

--- ibas_bahia_website/static/src/js/bahia_application_form.js
+++ ibas_bahia_website/static/src/js/bahia_application_form.js
@@ -33,136 +33,136 @@ odoo.define('ibas_bahia_website.apply_template', function(require){
 			var previous_employment_data = [];
 			var social_media_data = [];
 
-			// // Family
-			// var family_rows = $('.applicant_families > tbody > tr.family_line');
-			// _.each(family_rows, function(row) {
-			// 	let relationship = $(row).find('select[id="relationship"]').val();
-			// 	let full_name = $(row).find('input[id="full_name"]').val();
-			// 	let applicant_gender = $(row).find('select[id="applicant_gender"]').val();
-			// 	let date_of_birth = $(row).find('input[id="date_of_birth"]').val();
-			// 	let placeof_birth = $(row).find('input[id="family_placeof_birth"]').val();
-			// 	console.log(relationship, full_name)
-			// 	family_data.push({
-			// 		'relationship': relationship,
-			// 		'full_name': full_name,
-			// 		'gender': applicant_gender,
-			// 		'date_of_birth': date_of_birth,
-			// 		'placeof_birth': placeof_birth
-			// 	});
-			// });
-			// $('textarea[name="applicant_families"]').val(JSON.stringify(family_data));
+			// Family
+			var family_rows = $('.applicant_families > tbody > tr.family_line');
+			_.each(family_rows, function(row) {
+				let relationship = $(row).find('select[id="relationship"]').val();
+				let full_name = $(row).find('input[id="full_name"]').val();
+				let applicant_gender = $(row).find('select[id="applicant_gender"]').val();
+				let date_of_birth = $(row).find('input[id="date_of_birth"]').val();
+				let placeof_birth = $(row).find('input[id="family_placeof_birth"]').val();
+				console.log(relationship, full_name)
+				family_data.push({
+					'relationship': relationship,
+					'full_name': full_name,
+					'gender': applicant_gender,
+					'date_of_birth': date_of_birth,
+					'placeof_birth': placeof_birth
+				});
+			});
+			$('textarea[name="applicant_families"]').val(JSON.stringify(family_data));
 
-			// // Education
-			// var education_rows = $('.applicant_education > tbody > tr.education_line');
-			// _.each(education_rows, function(row) {
-			// 	let schooltype = $(row).find('select[id="schooltype"]').val();
-			// 	let name_school = $(row).find('input[id="name_school"]').val();
-			// 	let description = $(row).find('input[id="description"]').val();
-			// 	let date_from = $(row).find('input[id="date_from"]').val();
-			// 	let date_to = $(row).find('input[id="date_to"]').val();
-			// 	let school_address = $(row).find('input[id="school_address"]').val();
-			// 	console.log(schooltype, name_school)
-			// 	education_data.push({
-			// 		'schooltype': schooltype,
-			// 		'name_school': name_school,
-			// 		'description': description,
-			// 		'date_from': date_from,
-			// 		'date_to': date_to,
-			// 		'school_address': school_address
-			// 	});
-			// });
-			// $('textarea[name="applicant_education"]').val(JSON.stringify(education_data));
+			// Education
+			var education_rows = $('.applicant_education > tbody > tr.education_line');
+			_.each(education_rows, function(row) {
+				let schooltype = $(row).find('select[id="schooltype"]').val();
+				let name_school = $(row).find('input[id="name_school"]').val();
+				let description = $(row).find('input[id="description"]').val();
+				let date_from = $(row).find('input[id="date_from"]').val();
+				let date_to = $(row).find('input[id="date_to"]').val();
+				let school_address = $(row).find('input[id="school_address"]').val();
+				console.log(schooltype, name_school)
+				education_data.push({
+					'schooltype': schooltype,
+					'name_school': name_school,
+					'description': description,
+					'date_from': date_from,
+					'date_to': date_to,
+					'school_address': school_address
+				});
+			});
+			$('textarea[name="applicant_education"]').val(JSON.stringify(education_data));
 
-			// // Record Books
-			// var record_books_rows = $('.applicant_document_ids > tbody > tr.record_books_line');
-			// _.each(record_books_rows, function(row) {
-			// 	let document = $(row).find('select[id="document"]').val();
-			// 	let document_number = $(row).find('input[id="document_number"]').val();
-			// 	let date_issued = $(row).find('input[id="date_issued"]').val();
-			// 	let date_expiry = $(row).find('input[id="date_expiry"]').val();
-			// 	let issuing_authority = $(row).find('input[id="issuing_authority"]').val();
-			// 	let place_ofissue = $(row).find('input[id="place_ofissue"]').val();
-			// 	console.log(document, document_number)
-			// 	record_books_data.push({
-			// 		'document': document,
-			// 		'document_number': document_number,
-			// 		'date_issued': date_issued,
-			// 		'date_expiry': date_expiry,
-			// 		'issuing_authority': issuing_authority,
-			// 		'place_ofissue': place_ofissue
-			// 	});
-			// });
-			// $('textarea[name="applicant_document_ids"]').val(JSON.stringify(record_books_data));
+			// Record Books
+			var record_books_rows = $('.applicant_document_ids > tbody > tr.record_books_line');
+			_.each(record_books_rows, function(row) {
+				let document = $(row).find('select[id="document"]').val();
+				let document_number = $(row).find('input[id="document_number"]').val();
+				let date_issued = $(row).find('input[id="date_issued"]').val();
+				let date_expiry = $(row).find('input[id="date_expiry"]').val();
+				let issuing_authority = $(row).find('input[id="issuing_authority"]').val();
+				let place_ofissue = $(row).find('input[id="place_ofissue"]').val();
+				console.log(document, document_number)
+				record_books_data.push({
+					'document': document,
+					'document_number': document_number,
+					'date_issued': date_issued,
+					'date_expiry': date_expiry,
+					'issuing_authority': issuing_authority,
+					'place_ofissue': place_ofissue
+				});
+			});
+			$('textarea[name="applicant_document_ids"]').val(JSON.stringify(record_books_data));
 
-			// // References - Employed Relatives
-			// var employed_relative_rows = $('.applicant_employed_relatives_ids > tbody > tr.employed_relative_line');
-			// _.each(employed_relative_rows, function(row) {
-			// 	let name_of_crew = $(row).find('input[id="name_of_crew"]').val();
-			// 	let position_and_principal = $(row).find('input[id="position_and_principal"]').val();
-			// 	let relationship = $(row).find('select[id="relationship"]').val();
-			// 	console.log(name_of_crew, position_and_principal)
-			// 	employed_relative_data.push({
-			// 		'name_of_crew': name_of_crew,
-			// 		'position_and_principal': position_and_principal,
-			// 		'relationship': relationship
-			// 	});
-			// });
-			// $('textarea[name="applicant_employed_relatives_ids"]').val(JSON.stringify(employed_relative_data));
+			// References - Employed Relatives
+			var employed_relative_rows = $('.applicant_employed_relatives_ids > tbody > tr.employed_relative_line');
+			_.each(employed_relative_rows, function(row) {
+				let name_of_crew = $(row).find('input[id="name_of_crew"]').val();
+				let position_and_principal = $(row).find('input[id="position_and_principal"]').val();
+				let relationship = $(row).find('select[id="relationship"]').val();
+				console.log(name_of_crew, position_and_principal)
+				employed_relative_data.push({
+					'name_of_crew': name_of_crew,
+					'position_and_principal': position_and_principal,
+					'relationship': relationship
+				});
+			});
+			$('textarea[name="applicant_employed_relatives_ids"]').val(JSON.stringify(employed_relative_data));
 
-			// // References - Previous Application
-			// var previous_application_rows = $('.applicant_previous_application_ids > tbody > tr.previous_application_line');
-			// _.each(previous_application_rows, function(row) {
-			// 	let date_applied = $(row).find('input[id="date_applied"]').val();
-			// 	let job_applied_id = $(row).find('select[id="job_applied_id"]').val();
-			// 	console.log(date_applied, job_applied_id)
-			// 	previous_application_data.push({
-			// 		'date_applied': date_applied,
-			// 		'job_applied_id': job_applied_id
-			// 	});
-			// });
-			// $('textarea[name="applicant_previous_application_ids"]').val(JSON.stringify(previous_application_data));
+			// References - Previous Application
+			var previous_application_rows = $('.applicant_previous_application_ids > tbody > tr.previous_application_line');
+			_.each(previous_application_rows, function(row) {
+				let date_applied = $(row).find('input[id="date_applied"]').val();
+				let job_applied_id = $(row).find('select[id="job_applied_id"]').val();
+				console.log(date_applied, job_applied_id)
+				previous_application_data.push({
+					'date_applied': date_applied,
+					'job_applied_id': job_applied_id
+				});
+			});
+			$('textarea[name="applicant_previous_application_ids"]').val(JSON.stringify(previous_application_data));
 
-			// // References - Previous Employment
-			// var previous_employment_rows = $('.applicant_previous_employment_ids > tbody > tr.previous_employment_line');
-			// _.each(previous_employment_rows, function(row) {
-			// 	let rank_position = $(row).find('input[id="rank_position"]').val();
-			// 	let manning_agency = $(row).find('input[id="manning_agency"]').val();
-			// 	let employer_principal = $(row).find('input[id="employer_principal"]').val();
-			// 	let address_contact_info_manning_agen = $(row).find('input[id="address_contact_info_manning_agen"]').val();
-			// 	let vessel_name = $(row).find('input[id="vessel_name"]').val();
-			// 	let vessel_type = $(row).find('select[id="vessel_type"]').val();
-			// 	let grt = $(row).find('input[id="grt"]').val();
-			// 	let date_from = $(row).find('input[id="date_from"]').val();
-			// 	let date_to = $(row).find('input[id="date_to"]').val();
-			// 	let duties_and_responsibility = $(row).find('input[id="duties_and_responsibility"]').val();
-			// 	console.log(date_applied, job_applied_id)
-			// 	previous_employment_data.push({
-			// 		'rank_position': rank_position,
-			// 		'manning_agency': manning_agency,
-			// 		'employer_principal': employer_principal,
-			// 		'address_contact_info_manning_agen': address_contact_info_manning_agen,
-			// 		'vessel_name': vessel_name,
-			// 		'vessel_type': vessel_type,
-			// 		'grt': grt,
-			// 		'date_from': date_from,
-			// 		'date_to': date_to,
-			// 		'duties_and_responsibility': duties_and_responsibility
-			// 	});
-			// });
-			// $('textarea[name="applicant_previous_employment_ids"]').val(JSON.stringify(previous_employment_data));
+			// References - Previous Employment
+			var previous_employment_rows = $('.applicant_previous_employment_ids > tbody > tr.previous_employment_line');
+			_.each(previous_employment_rows, function(row) {
+				let rank_position = $(row).find('input[id="rank_position"]').val();
+				let manning_agency = $(row).find('input[id="manning_agency"]').val();
+				let employer_principal = $(row).find('input[id="employer_principal"]').val();
+				let address_contact_info_manning_agen = $(row).find('input[id="address_contact_info_manning_agen"]').val();
+				let vessel_name = $(row).find('input[id="vessel_name"]').val();
+				let vessel_type = $(row).find('select[id="vessel_type"]').val();
+				let grt = $(row).find('input[id="grt"]').val();
+				let date_from = $(row).find('input[id="date_from"]').val();
+				let date_to = $(row).find('input[id="date_to"]').val();
+				let duties_and_responsibility = $(row).find('input[id="duties_and_responsibility"]').val();
+				console.log(date_applied, job_applied_id)
+				previous_employment_data.push({
+					'rank_position': rank_position,
+					'manning_agency': manning_agency,
+					'employer_principal': employer_principal,
+					'address_contact_info_manning_agen': address_contact_info_manning_agen,
+					'vessel_name': vessel_name,
+					'vessel_type': vessel_type,
+					'grt': grt,
+					'date_from': date_from,
+					'date_to': date_to,
+					'duties_and_responsibility': duties_and_responsibility
+				});
+			});
+			$('textarea[name="applicant_previous_employment_ids"]').val(JSON.stringify(previous_employment_data));
 
-			// // Social Media
-			// var social_media_rows = $('.applicant_social_media > tbody > tr.social_media_line');
-			// _.each(social_media_rows, function(row) {
-			// 	let socialmedia_id = $(row).find('select[id="socialmedia_id"]').val();
-			// 	let name = $(row).find('input[id="name"]').val();
-			// 	console.log(socialmedia_id, name)
-			// 	social_media_data.push({
-			// 		'socialmedia_id': socialmedia_id,
-			// 		'name': name,
-			// 	});
-			// });
-			// $('textarea[name="applicant_socialmedia_ids"]').val(JSON.stringify(social_media_data));
+			// Social Media
+			var social_media_rows = $('.applicant_social_media > tbody > tr.social_media_line');
+			_.each(social_media_rows, function(row) {
+				let socialmedia_id = $(row).find('select[id="socialmedia_id"]').val();
+				let name = $(row).find('input[id="name"]').val();
+				console.log(socialmedia_id, name)
+				social_media_data.push({
+					'socialmedia_id': socialmedia_id,
+					'name': name,
+				});
+			});
+			$('textarea[name="applicant_socialmedia_ids"]').val(JSON.stringify(social_media_data));
 
 		},
 
