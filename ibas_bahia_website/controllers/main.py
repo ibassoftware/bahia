d065f3ceb182677b845e1f8550872e92aec44c18
Reynaflor Sentillas <reynaflorsentillas@ymail.com>
2024-01-26 18:09:29 +0800

    Update main.py

--- ibas_bahia_website/controllers/main.py
+++ ibas_bahia_website/controllers/main.py
@@ -84,6 +84,7 @@ class BahiasApplicationForm(http.Controller):
 		duplicate_applicant_id = request.env['hr.applicant'].sudo().search([('name', '=', applicant_name), ('date_of_birth', '=', applicant_date_of_birth), ('email_from', '=', applicant_email_from)])
 		
 		if duplicate_applicant_id:
+			_logger.info("Duplicate application!")
 			error = _("Duplicate application! One applicant, one application only.")
 			return json.dumps({
 				'error': error,
