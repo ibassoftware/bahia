303ae8233975387244ef734c8f487fc2a7ffc7b2
Reynaflor Sentillas <reynaflorsentillas@ymail.com>
2024-01-30 00:20:24 +0800

    Update bahia_application_form.js

--- ibas_bahia_website/static/src/js/bahia_application_form.js
+++ ibas_bahia_website/static/src/js/bahia_application_form.js
@@ -167,7 +167,8 @@ odoo.define('ibas_bahia_website.apply_template', function(require){
 
 		_onClickRemove_family_line: function(ev){
 			console.log("remove_family")
-			$(this).parents("tr").remove();
+			var $row = $(this).closest("tr");
+			console.log($row)
 		},
 
 		_onClickAdd_family: function(ev){
